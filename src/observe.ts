1e43eaa97f8ba0054ecdd805a1dafab9052be08b
William Troup <williamtroup@gmail.com>
2024-07-16 11:04:59 +0100

    Added start of private functions.

--- src/observe.ts
+++ src/observe.ts
@@ -10,9 +10,90 @@
  * @copyright   Bunoon 2024
  */
 
+import { Is } from "./ts/is";
+import { type Configuration } from "./ts/type";
+
+
+type StringToJson = {
+    parsed: boolean;
+    object: any;
+};
+
 
 ( () => {
+    // Variables: Configuration
+    let _configuration: Configuration = {} as Configuration;
 
+
+    
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Triggering Custom Events
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function fireCustomTriggerEvent( triggerFunction: Function, ...args : any[] ) : void {
+        if ( Is.definedFunction( triggerFunction ) ) {
+            triggerFunction.apply( null, [].slice.call( args, 0 ) );
+        }
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Default Parameter/Option Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function getObjectFromString( objectString: any ) : StringToJson {
+        const result: StringToJson = {
+            parsed: true,
+            object: null
+        } as StringToJson;
+
+        try {
+            if ( Is.definedString( objectString ) ) {
+                result.object = JSON.parse( objectString );
+            }
+
+        } catch ( e1: any ) {
+            try {
+                result.object = eval( `(${objectString})` );
+
+                if ( Is.definedFunction( result.object ) ) {
+                    result.object = result.object();
+                }
+                
+            } catch ( e2: any ) {
+                if ( !_configuration.safeMode ) {
+                    logError( _configuration.text!.objectErrorText!.replace( "{{error_1}}",  e1.message ).replace( "{{error_2}}",  e2.message ) );
+                    result.parsed = false;
+                }
+                
+                result.object = null;
+            }
+        }
+
+        return result;
+    }
+
+    function logError( error: string ) : boolean {
+        let result: boolean = true;
+
+        if ( !_configuration.safeMode ) {
+            console.error( error );
+            result = false;
+        }
+
+        return result;
+    }
+
+    
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Initialize Observe.js
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
 
     ( () => {
 
--- src/ts/type.ts
+++ src/ts/type.ts
@@ -14,6 +14,7 @@
 export type Configuration = {
     safeMode?: boolean;
     domElementTypes?: string[] | string;
+    text?: ConfigurationText;
 };
 
 export type ConfigurationText = {
