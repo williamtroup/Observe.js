6df14d1d4dc12656d1aa4e8446ebcdb46f227c90
William Troup <williamtroup@gmail.com>
2024-07-16 10:47:13 +0100

    Added constant.ts and start of observe.ts.

---
+++ src/observe.ts
@@ -0,0 +1,20 @@
+/**
+ * Observe.js
+ * 
+ * A lightweight JavaScript library that allows developers to keep track of changes to JavaScript objects and/or DOM elements.
+ * 
+ * @file        observe.ts
+ * @version     v1.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+( () => {
+
+
+    ( () => {
+
+    } )();
+} )();
